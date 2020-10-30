bb42e01b1bcd336d8406c2af6daece34d289e7f1
Rafael Osorio <rafa.osorioa@gmail.com>
2020-10-30 18:16:16 -0500

    Created homepage an styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import Header from "./components";
+import "./default.scss";
+import Homepage from "./pages/Homepage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
+    </div>
+  );
 }
 
 export default App;
